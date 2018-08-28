a18b176de3a84f97ca6c5f8359e78fc2c40f649c
지수 김 <rhlcp23@naver.com>
2018-08-28 16:19:07 +0900

    파일추가

---
+++ Source/ProjectJ/Monster/ArcherMonster/ArcherAnimInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ArcherAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTJ_API UArcherAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/ProjectJ/State/ArcherState/ArcherState.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+//UENUM(BlueprintType)
+//enum class EArcherState : uint8
+//{
+//	Idle = 0 UMETA(DisplayName = "Idle"),
+//	Patrol = 1 UMETA(DisplayName = "Patrol"),
+//	Chase = 2 UMETA(DisplayName = "Chase"),
+//	Battle = 3 UMETA(DisplayName = "Battle"),
+//	Beaten = 4 UMETA(DisplayName = "Beaten"),
+//	Dead = 5 UMETA(DisplayName = "Dead")
+//};
+//
+//UENUM(BlueprintType)
+//enum class EArcherAnimState : uint8
+//{
+//	Wait = 0 UMETA(DisplayName = "Wait"),
+//	Walk = 1 UMETA(DisplayName = "Walk"),
+//	Run = 2 UMETA(DisplayName = "Run"),
+//	Beaten = 3 UMETA(DisplayName = "Beaten"),
+//	Attack = 4 UMETA(DisplayName = "Attack")
+//};
+
