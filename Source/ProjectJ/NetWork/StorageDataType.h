746bf857b8cb9d670243dcf6d24999606fc6e241
EverStay <neaidgi@gmail.com>
2018-12-20 00:48:21 +0900

    스토리지 데이터 타입 추가

--- Source/ProjectJ/NetWork/StorageDataType.h
+++ Source/ProjectJ/NetWork/StorageDataType.h
@@ -22,4 +22,31 @@ struct CharacterInfo
 	float z;
 };
 
+class Character
+{
+private:
+public:
+	char* character_name;			// 캐릭터 닉네임
+	int character_code;				// 코드
+	int	character_str;				// 힘
+	int	character_dex;				// 민
+	int	character_int;				// 지
+	int	character_health;			// 체력
+	int	character_mana;				// 마나
+	int	character_attackpoint;		// 공격력
+	int	character_defensepoint;		// 방어력
+	int	character_speed;			// 이속
+
+	void SetCharacter_Name(char* _character_name)
+	{
+		int len = strlen(_character_name);
+		char* name = new char[len + 1];
+		memset(name, 0, len + 1);
+		strcpy_s(name, len + 1, _character_name);
+		character_name = name;
+	}
+
+	~Character() { delete[]character_name; }
+}
+
 #endif
