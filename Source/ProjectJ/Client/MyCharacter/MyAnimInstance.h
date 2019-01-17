dd0fb5c6b6358475cc179b7dae675b0d86cca408
YamSaeng <tyhg56@naver.com>
2019-01-18 01:56:48 +0900

    총잡이 자동 빙의 제거, 주석 다는중

--- Source/ProjectJ/Client/MyCharacter/MyAnimInstance.h
+++ Source/ProjectJ/Client/MyCharacter/MyAnimInstance.h
@@ -6,8 +6,8 @@
 #include "Animation/AnimInstance.h"
 #include "MyAnimInstance.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAttackEndedDelegate);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnComboSaveDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAttackEndedDelegate); //공격 애니메이션 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnComboSaveDelegate); //콤보 저장 애니메이션 델리게이트
 
 /**
  * 
@@ -18,33 +18,33 @@ class PROJECTJ_API UMyAnimInstance : public UAnimInstance
 	GENERATED_BODY()
 private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Velocity, Meta = (AllowPrivateAccess = true))
-		float Speed;
+		float Speed; //캐릭터의 속력
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Velocity, Meta = (AllowPrivateAccess = true))
-		float Direction;
+		float Direction; //캐릭터의 현재 방향
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = IsRun, Meta = (AllowPrivateAccess = true))
-		bool IsAccelerating;
+		bool IsAccelerating; //가속 중인지 아닌지
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = IsJump, Meta = (AllowPrivateAccess = true))
-		bool IsJump;
+		bool IsJump; //점프 중인지 아닌지
 protected:
 	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = Montage)
-		class UAnimMontage* ClickedReactionMontage;
+		class UAnimMontage* ClickedReactionMontage; //캐릭터 생성창에서 선택시 출력해줄 애니메이션
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Montage)
-		class UAnimMontage* LevelStartMontage;
+		class UAnimMontage* LevelStartMontage; //캐릭터 선택창에서 슬롯 선택시 출력해줄 애니메이션
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Montage)
-		class UAnimMontage* AttackMontage;
+		class UAnimMontage* AttackMontage; //캐릭터 공격애니메이션 
 
-	FName GetAttackMontageSection(int32 NewSection);
+	FName GetAttackMontageSection(int32 NewSection); //캐릭터 공격애니메이션
 
-	virtual void PlayClickedReactionMontage();
-	virtual void PlayLevelStartMontage();
-	virtual void PlayAttackMontage();
-	virtual void JumpAttackMontageSection(int32 NewSection);
+	virtual void PlayClickedReactionMontage(); //캐릭터 생성창 선택 애니메이션 실행
+	virtual void PlayLevelStartMontage(); //캐릭터 선택창 슬롯 선택 애니메이션 실행
+	virtual void PlayAttackMontage(); //공격 애니메이션 실행
+	virtual void JumpAttackMontageSection(int32 NewSection); //공격 애니메이션 몽타주 섹션 점프 해주는 함수
 public:
-	FOnAttackEndedDelegate OnAttackEnded;
-	FOnComboSaveDelegate OnComboSave;
+	FOnAttackEndedDelegate OnAttackEnded; //공격 애니메이션 끝 델리게이트 변수
+	FOnComboSaveDelegate OnComboSave; //콤보 저장 델리게이트 변수
 
-	UMyAnimInstance();
+	UMyAnimInstance(); //생성자
 
 	UFUNCTION()
-	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override; // 틱 함수
 };
