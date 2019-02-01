66f1d894cd473dbe781371bad5ae381cc642ab86
khy3309 <khy3309@naver.com>
2019-02-01 13:00:11 +0900

    임시  IP 수정

--- Source/ProjectJ/NetWork/Global.h
+++ Source/ProjectJ/NetWork/Global.h
@@ -155,8 +155,8 @@ enum RESULT {
 #define PRODUCT_NAMESIZE 50
 #define ENCRYPT_KEY 951324896
 //#define SERVERIP "127.0.0.1"
-#define SERVERIP "14.36.166.64"
-//#define SERVERIP "182.227.211.86"
+#define SERVERIP "121.176.115.205"
 //#define SERVERIP "14.36.166.64"
+//#define SERVERIP "182.227.211.86"
 
 #endif   
