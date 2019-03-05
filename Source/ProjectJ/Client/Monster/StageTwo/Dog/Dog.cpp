b41ba2465b65a3e066adbac162a8fee443db9ade
세중 김 <dodrv@naver.com>
2019-03-05 19:01:36 +0900

    스테이지 2 추가

---
+++ Source/ProjectJ/Client/Monster/StageTwo/Dog/Dog.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Dog.h"
+
+
+ADog::ADog()
+{
+
+}
+
+//void ADog::BeginPlay()
+//{
+//	Super::BeginPlay();
+//
+//	MaxHP = 100.0f;
+//	CurrentHP = MaxHP;
+//
+//	TargetLimitDistance = 150.0f;
+//}
