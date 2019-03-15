7540cc80b4025d1cc7bfaf0c97c3ba479f0b6499
세중 김 <dodrv@naver.com>
2019-03-15 12:09:36 +0900

    Dog AIController / Animinstance 변경

--- Source/ProjectJ/Client/Monster/StageTwo/Dog/DogAnimInstance.h
+++ Source/ProjectJ/Client/Monster/StageTwo/Dog/DogAnimInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Client/Monster/MonsterAnimInstance.h"
+#include "Client/State/MonsterState/MonsterState.h"
 #include "DogAnimInstance.generated.h"
 
 /**
@@ -15,6 +16,17 @@ class PROJECTJ_API UDogAnimInstance : public UMonsterAnimInstance
 	GENERATED_BODY()
 public:
 	UDogAnimInstance();
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = State, Meta = (AllowPrivateAccess = true))
+		EDogState CurrentState;
+	UFUNCTION()
+		void AnimNotify_MonsterAttackHit(UAnimNotify* Notify);
+	//UFUNCTION()
+	//	void AnimNotify_MonsterSaveAttack(UAnimNotify* Notify);
+	//UFUNCTION()
+	//	void ANimNotify_MonsterAttackEnded();
+	UFUNCTION()
+		void AnimNotify_Death(UAnimNotify* Notify);
 	
 	
 	
