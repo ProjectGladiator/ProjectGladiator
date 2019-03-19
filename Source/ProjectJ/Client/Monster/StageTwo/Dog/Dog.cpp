2768250573266ab70dbe0ba5526a72c04b2d6a73
세중 김 <dodrv@naver.com>
2019-03-19 14:41:47 +0900

    Dog cpp 주석

--- Source/ProjectJ/Client/Monster/StageTwo/Dog/Dog.cpp
+++ Source/ProjectJ/Client/Monster/StageTwo/Dog/Dog.cpp
@@ -4,32 +4,37 @@
 
 #pragma region ClientHeader
 #include "UObject/ConstructorHelpers.h" // 경로 탐색 헤더
-#include "DogAIController.h" //곰 AI 컨트롤러 헤더
+#include "DogAIController.h" //AI 컨트롤러 헤더
 #include "Components/SkeletalMeshComponent.h" //스켈레탈 메쉬 헤더
 #include "Kismet/GameplayStatics.h" //각종 유틸 헤더
 #include "Client/Monster/Manager/AIManager.h" //AI담당 컴포넌트 헤더
 #include "Client/MyCharacter/PC/MyCharacter.h" //캐릭터들 부모
 #include "GameFrameWork/CharacterMovementComponent.h" //캐릭터 속도 관련 헤더
-#include "DogAnimInstance.h" //곰 애님 인스턴스 헤더
+#include "DogAnimInstance.h" //애님 인스턴스 헤더
 #include "Kismet/KismetSystemLibrary.h"  //라인 트레이스 헤더 관련 헤더
 #include "Kismet/KismetMathLibrary.h" //수학 관련 헤더
 #include "Components/CapsuleComponent.h" //캡슐 컴포넌트 헤더
 #pragma endregion 클라이언트 헤더
+
 #pragma region ServerHeader
 //Some ServerHeader
 #pragma endregion 서버 헤더
+
 ADog::ADog()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	AIControllerClass = ADogAIController::StaticClass();
 
+#pragma region SkeletalMesh Set
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh>SK_Dog(TEXT("SkeletalMesh'/Game/Assets/Monster/QuadrapedCreatures/Barghest/Meshes/SK_BARGHEST.SK_BARGHEST'"));
 
 	if (SK_Dog.Succeeded())
 	{
 		GetMesh()->SetSkeletalMesh(SK_Dog.Object);
 	}
+#pragma endregion 스켈레탈 메쉬 세팅
 
+#pragma region AnimBlueprint Set
 	// 애님블루프린트 찾아 메쉬에 세팅
 	static ConstructorHelpers::FObjectFinder<UClass>ABP_Dog(TEXT("AnimBlueprint'/Game/Blueprints/Monster/StageTwo/Dog/Blueprints/ABP_Dog.ABP_Dog_C'"));
 
@@ -44,10 +49,13 @@ ADog::ADog()
 			GetMesh()->SetAnimInstanceClass(DogAnimBlueprint);  //찾은 애니메이션블루프린트를 넣어준다.
 		}
 	}
+#pragma endregion 애님 블루프린트 메쉬 세팅
 
+#pragma region Mesh Set To World pos
 	GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, -90.0f));
 	GetMesh()->SetRelativeScale3D(FVector(1.0f, 1.0f, 1.0f));
 	GetMesh()->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+#pragma endregion 매쉬를 월드에 적용하기 위한 위치값
 
 	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
 }
@@ -56,6 +64,7 @@ void ADog::BeginPlay()
 {
 	Super::BeginPlay();
 
+#pragma region HP, TagetLimit_Distance, AttackRange, State Set
 	MaxHP = 100.0f;
 	CurrentHP = MaxHP;
 
@@ -64,6 +73,7 @@ void ADog::BeginPlay()
 	AttackInfo.SetAttackInfo(50.0f, 80.0f, 120.0f);
 
 	CurrentState = EDogState::Idle;
+#pragma endregion 수치로 표현되는 초기화 값 모음
 
 	Target = Cast<AMyCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
 
@@ -81,19 +91,29 @@ void ADog::BeginPlay()
 
 void ADog::Tick(float DeltaTime)
 {
+	//타겟이 존재하면
 	if (Target)
 	{
+		//타겟과의 거리 계산 return 값으로 (float)Distance가 반환
 		float Distance = AIManager->DistanceCalculate(this, Target);
 
+		//현재 State상태에 따라 변동 FSM
+#pragma region Custom_FSM
 		switch (CurrentState)
 		{
+			//대기 상태
 		case EDogState::Idle:
+		{
 			CurrentState = EDogState::Chase;
+		}
 			break;
+			//추적 상태	
 		case EDogState::Chase:
 		{
+			//거리에 따른 추적 상태 변환 문구
 			EPathFollowingRequestResult::Type GoalResult = AIManager->TargetChase(DogAIController, Target, TargetLimitDistance);
 
+			//추적 상태 변화값
 			switch (GoalResult)
 			{
 			case EPathFollowingRequestResult::AlreadyAtGoal:
@@ -110,16 +130,19 @@ void ADog::Tick(float DeltaTime)
 			}
 
 		}
-		break;
+			break;
+			//공격 상태
 		case EDogState::Attack:
 		{
+			// 콤보 존재시 공격중에 콤보활성화 bool문
 			if (IsAttack)
 			{
 				IsCombo = true;
 			}
+			//처음 공격시 활성화 문
 			else
 			{
-				GLog->Log(FString::Printf(TEXT("곰 처음 공격")));
+				GLog->Log(FString::Printf(TEXT("개 처음 공격")));
 				IsAttack = true;
 
 			/*	if (DogAnimInstance)
@@ -130,6 +153,7 @@ void ADog::Tick(float DeltaTime)
 				}*/
 			}
 
+#pragma region  RotateToTarget
 			FRotator DogRotator = GetActorRotation();
 			FRotator LooAtRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), Target->GetActorLocation());
 
@@ -145,9 +169,12 @@ void ADog::Tick(float DeltaTime)
 				IsCombo = false;
 				CurrentState = EDogState::Chase;
 			}
+#pragma endregion 공격시 타겟을 향해 회전
 		}
-		break;
+			break;
+			//죽은 상태
 		case EDogState::Death:
+		{
 			DeathInVisibleValue += 0.01;
 			GetMesh()->SetScalarParameterValueOnMaterials(TEXT("Amount"), DeathInVisibleValue);
 
@@ -155,13 +182,17 @@ void ADog::Tick(float DeltaTime)
 			{
 				Destroy();
 			}
+		}
 			break;
 		}
+#pragma endregion FMS (상태에 따라 행동변화)
 	}
+	//타겟이 없다면
 	else
 	{
 		GLog->Log(FString::Printf(TEXT("개 : 타겟이 존재하지 않음")));
 	}
+
 }
 
 void ADog::SetAIController(AMonsterAIController * NewAIController)
