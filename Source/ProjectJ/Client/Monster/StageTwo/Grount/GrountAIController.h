d60988336174b43c9fac9da95bad217c6e933ddf
세중 김 <dodrv@naver.com>
2019-03-19 19:17:55 +0900

    고블린 몬스터 준비

---
+++ Source/ProjectJ/Client/Monster/StageTwo/Grount/GrountAIController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Client/Monster/MonsterAIController.h"
+#include "GrountAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTJ_API AGrountAIController : public AMonsterAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
