5bf591497d4c23ac6394d65e0e25a762b596492e
세중 김 <dodrv@naver.com>
2019-03-28 19:38:31 +0900

    Grount 코드 추가

---
+++ Source/ProjectJ/Client/Monster/StageTwo/Grount/Grount.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Client/Monster/Monster.h"
+#include "Grount.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTJ_API AGrount : public AMonster
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
