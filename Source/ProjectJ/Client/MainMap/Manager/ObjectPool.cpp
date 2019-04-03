a4f1e64466306213ffec5cf1aad2bd211da6b70c
세중 김 <dodrv@naver.com>
2019-04-03 17:42:58 +0900

    오브젝트 풀 SpawnParameter 추가로 문제 해결

--- Source/ProjectJ/Client/MainMap/Manager/ObjectPool.cpp
+++ Source/ProjectJ/Client/MainMap/Manager/ObjectPool.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+Ôªø// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ObjectPool.h"
 
@@ -14,7 +14,7 @@
 // Sets default values
 AObjectPool::AObjectPool()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
 	// create the Box Component the spawn volume
@@ -25,12 +25,10 @@ AObjectPool::AObjectPool()
 
 	//Set SpawnNum 
 	//FullPoolVolume = initPoolVolume;
+	SetStaticMonsterClass();
 
 	//Test to Use this pool check
 	isTestPoolStart = false;
-
-	//Set Spawn Monster
-	SetStaticMonsterClass();
 
 	//Current pool init is 0
 	/*currentPool_count = 0;*/
@@ -40,6 +38,8 @@ AObjectPool::AObjectPool()
 void AObjectPool::BeginPlay()
 {
 	Super::BeginPlay();
+
+
 	GLog->Log(FString::Printf(TEXT("AObjectPool BeginPlay")));
 	//Set Pool volume to BeginPlay
 	FullPoolVolume = initPoolVolume;
@@ -65,14 +65,11 @@ FVector AObjectPool::GetRandomPointInVolume()
 
 void AObjectPool::PoolSetting()
 {
-	ABear* Bear;
-	ADinosaur* Dinosaur;
-	//Get World
-	UWorld* const world = GetWorld();
-
 	FVector SpawnPos_Vector;
+
 	FActorSpawnParameters SpawnActorOption;
 	SpawnActorOption.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
 	//	//Set ths Spawn Paramaters
 	//FActorSpawnParameters SpawnParams;
 	//SpawnParams.Owner = this;
@@ -87,60 +84,38 @@ void AObjectPool::PoolSetting()
 		SpawnPos_Vector = FVector::ZeroVector;
 	}
 
-	
 	for (int i_spawnObject = 0; i_spawnObject < FullPoolVolume; i_spawnObject++)
 	{
-		AMonster* SpawnActor=nullptr;
+		AMonster* SpawnActor = nullptr;
+
 		if (i_spawnObject % 2 == 0)
 		{
-			//æÓ∂≤ Monster∏¶ ªÁøÎ«“ ∞«¡ˆ ¡§«œ¥¬ ∫Œ∫–¿ª ∏∏µÈæÓæﬂ«‘
-			SpawnActor = GetWorld()->SpawnActor<ABear>(Bear->StaticClass(), SpawnPos_Vector, FRotator::ZeroRotator, SpawnActorOption);
-			
-			if (SpawnActor)
-			{
-				GLog->Log(FString::Printf(TEXT("Ω∫∆˘ æ◊≈Õ ¿÷¿Ω")));
-
-				//Pawn->Auto Possess AI
-				//SpawnActor->AutoPossessAI = EAutoPossessAI::Spawned;
-				Spawn_Array.Emplace(SpawnActor);
-				UE_LOG(LogTemp, Warning, TEXT("SpawnActor Create"));
-
-				SpawnObject_SetActive(Spawn_Array[i_spawnObject], true);
-				//Spawn_Array[i_spawnObject]->bisActive = false;
-			}
-			else
-			{
-				GLog->Log(FString::Printf(TEXT("Ω∫∆˘ æ◊≈Õ NULL")));
-			}
+			SpawnActor = GetWorld()->SpawnActor<ABear>(whatToSpawn_Array[0], SpawnPos_Vector, FRotator::ZeroRotator, SpawnActorOption);
 		}
 		else 
 		{
-			//æÓ∂≤ Monster∏¶ ªÁøÎ«“ ∞«¡ˆ ¡§«œ¥¬ ∫Œ∫–¿ª ∏∏µÈæÓæﬂ«‘
-			SpawnActor = GetWorld()->SpawnActor<ADinosaur>(Dinosaur->StaticClass(), SpawnPos_Vector, FRotator::ZeroRotator, SpawnActorOption);
+			SpawnActor = GetWorld()->SpawnActor<ADinosaur>(whatToSpawn_Array[1], SpawnPos_Vector, FRotator::ZeroRotator, SpawnActorOption);
+		}
 
-			if (SpawnActor)
-			{
-				GLog->Log(FString::Printf(TEXT("Ω∫∆˘ æ◊≈Õ ¿÷¿Ω")));
-
-				//Pawn->Auto Possess AI
-				//SpawnActor->AutoPossessAI = EAutoPossessAI::Spawned;
-				Spawn_Array.Emplace(SpawnActor);
-				UE_LOG(LogTemp, Warning, TEXT("SpawnActor Create"));
-
-				SpawnObject_SetActive(Spawn_Array[i_spawnObject], true);
-				//Spawn_Array[i_spawnObject]->bisActive = false;
-			}
-			else
-			{
-				GLog->Log(FString::Printf(TEXT("Ω∫∆˘ æ◊≈Õ NULL")));
-			}
+		if (SpawnActor)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("SpawnActor Create"));
+			//Pawn->Auto Possess AI
+			//SpawnActor->AutoPossessAI = EAutoPossessAI::Spawned;
+			Spawn_Array.Emplace(SpawnActor);
+			SpawnObject_SetActive(Spawn_Array[i_spawnObject], true);
+			Spawn_Array[i_spawnObject]->bisActive = false;
+		}
+		else
+		{
+			GLog->Log(FString::Printf(TEXT("SpawnActor NULL")));
 		}
 	}
 }
 
 void AObjectPool::Pooling(int _counter)
 {
-	// ø‰±‚ø°¥¬ Pooling¿ª «“ ≥ªøÎ¿ª ¿˚æÓ¡÷∏ÈµÀπÃ¥Ÿ.
+	// ÏöîÍ∏∞ÏóêÎäî PoolingÏùÑ Ìï† ÎÇ¥Ïö©ÏùÑ Ï†ÅÏñ¥Ï£ºÎ©¥Îê©ÎØ∏Îã§.
 	if (_counter > FullPoolVolume)
 	{
 		//Error or FullPoolVolume Use
@@ -160,20 +135,27 @@ void AObjectPool::Pooling(int _counter)
 
 void AObjectPool::SpawnObject_SetActive(AMonster* SpawnObject, bool _bActive)
 {
-	if (_bActive == true)
+	if (SpawnObject)
 	{
-		SpawnObject->SetActorHiddenInGame(false);
-		SpawnObject->SetActorEnableCollision(true);
-		SpawnObject->SetActorTickEnabled(true);
+		if (_bActive == true)
+		{
+			SpawnObject->SetActorHiddenInGame(false);
+			SpawnObject->SetActorEnableCollision(true);
+			SpawnObject->SetActorTickEnabled(true);
+		}
+		else
+		{
+			// Hides visible components
+			SpawnObject->SetActorHiddenInGame(true);
+			// Disables collision components
+			SpawnObject->SetActorEnableCollision(false);
+			// Stops the Actor from ticking
+			SpawnObject->SetActorTickEnabled(false);
+		}
 	}
 	else
 	{
-		// Hides visible components
-		SpawnObject->SetActorHiddenInGame(true);
-		// Disables collision components
-		SpawnObject->SetActorEnableCollision(false);
-		// Stops the Actor from ticking
-		SpawnObject->SetActorTickEnabled(false);
+		GLog->Log(FString::Printf(TEXT("ÌôúÏÑ±Ìôî ÏÑ†ÌÉùÌïú Î™¨Ïä§ÌÑ∞Í∞Ä Ï°¥Ïû¨ÌïòÏßÄ ÏïäÏùå")));
 	}
 }
 
@@ -186,25 +168,7 @@ void AObjectPool::SetStaticMonsterClass()
 	UClass* bear = ABear::StaticClass();
 	UClass* Dinosaur = ADinosaur::StaticClass();
 
-	if (bear)
-	{
-		GLog->Log(FString::Printf(TEXT("bear ¿÷¿Ω")));
-	}
-	else
-	{
-		GLog->Log(FString::Printf(TEXT("bear ¿÷¿Ω")));
-	}
-
-	if (Dinosaur)
-	{
-		GLog->Log(FString::Printf(TEXT("Dinosaur ¿÷¿Ω")));
-	}
-	else
-	{
-		GLog->Log(FString::Printf(TEXT("Dinosaur ¿÷¿Ω")));
-	}
-
-	whatToSpawn_Array.Add(bear);
 	whatToSpawn_Array.Add(Dinosaur);
+	whatToSpawn_Array.Add(bear);
 }
 
