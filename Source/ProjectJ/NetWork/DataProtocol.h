517bc7d888ce0640a0cbf3e4b32c93feb983de2b
khy3309 <khy3309@naver.com>
2019-04-24 19:08:25 +0900

    몬스터 스폰 시간 추가

--- Source/ProjectJ/NetWork/DataProtocol.h
+++ Source/ProjectJ/NetWork/DataProtocol.h
@@ -49,6 +49,7 @@ namespace DataProtocol {
 		PGAMEDATA_PARTY_DUNGEON_STAGE_ENTER_RESULT,		// 파티 스테이지 입장  - (유저 숫자)
 		PGAMEDATA_PARTY_DUNGEON_STAGE_SPAWNINFO,		// 파티 스테이지 입장시 좌표 - ([char]코드,좌표)
 		PGAMEDATA_STAGE_MONSTER_TPYES_COUNT,			// 스테이지 몬스터 종류의 갯수가 몇개인지 - ([int] 몬스터 종류가 몇마리인지)
+		PGAMEDATA_STAGE_MONSTER_SPAWN_TIME,				// 스테이지 몬스터 스폰 시간 - ([int] 몬스터 스폰시간)
 		PGAMEDATA_STAGE_MONSTER_INFO,					// 스테이지 몬스터 정보 - ([int] 몬스터코드,[int] 몬스터숫자, [float*3] 좌표)
 		PGAMEDATA_MONSTER_MOVE_INFO						// 몬스터 이동정보 - ([int] 몬스터코드, [int] 몬스터숫자, [float*3] 좌표)
 	};
