bddf8c744ab61f685b9185e9f7c3cc33309afd60
세중 김 <dodrv@naver.com>
2019-04-26 17:01:44 +0900

    몬스터 Position의 Gap 만큼 출력하도록 로그 수정

--- Source/ProjectJ/Client/MainMap/MainMapGameMode.cpp
+++ Source/ProjectJ/Client/MainMap/MainMapGameMode.cpp
@@ -1120,10 +1120,14 @@ void AMainMapGameMode::Get_MonsterPosition(TMap<MONSTER_CODE, FMonsterstruct>_Mo
 	}
 	else
 	{
-		//UE_LOG(LogTemp, Warning, TEXT("before MonsterPosition: %d"),_MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation());
-		GLog->Log(FString::Printf(TEXT("before MonsterPosition: %d,%d,%d"),_MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().X, _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().Y, _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().Z));
+		FVector TempbeforeMonsterPosition = _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation();
+		GLog->Log(FString::Printf(TEXT("before MonsterPosition: %f,%f,%f"),_MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().X, _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().Y, _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().Z));
+		
 		_MonsterMapData[_MC].Monster_Volum_Array[_Num]->SetActorLocation(_MosterPosition);
-		GLog->Log(FString::Printf(TEXT("After MonsterPosition: %d,%d,%d"), _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().X, _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().Y, _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().Z));
-		//UE_LOG(LogTemp, Warning, TEXT("After MonsterPosition: %d"), _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation());
+
+		FVector TempAfterMonsterPosition = _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation();
+		GLog->Log(FString::Printf(TEXT("After MonsterPosition: %f,%f,%f"), _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().X, _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().Y, _MonsterMapData[_MC].Monster_Volum_Array[_Num]->GetActorLocation().Z));
+		
+		UE_LOG(LogTemp, Warning, TEXT("Gap MonsterPosition X: %d, Y: %d, Z: %d"), TempbeforeMonsterPosition.X - TempAfterMonsterPosition.X, TempbeforeMonsterPosition.Y - TempAfterMonsterPosition.Y, TempbeforeMonsterPosition.Z - TempAfterMonsterPosition.Z);
 	}
 }
