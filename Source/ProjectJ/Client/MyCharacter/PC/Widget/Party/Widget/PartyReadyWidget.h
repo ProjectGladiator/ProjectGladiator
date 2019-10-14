4e4bd9d12f3473b4c12cd8bac9f09823381f565a
성원 정 <tyhg56@naver.com>
2019-10-14 20:19:15 +0900

    파티 리더 게임시작 위젯 추가 ( 내용 아직 )

---
+++ Source/ProjectJ/Client/MyCharacter/PC/Widget/Party/Widget/PartyReadyWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PartyReadyWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTJ_API UPartyReadyWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
